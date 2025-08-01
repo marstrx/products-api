59738812b5888059c9f7761179484758fbd7bb09
Marouane Bachar <marstrone@gmail.com>
2025-08-01 21:35:15 +0200

    auth controller added

---
+++ controllers/auth-controller.js
@@ -0,0 +1,16 @@
+const User = require("../models/user");
+
+
+const registerController = async (req,res)=>{
+
+}
+
+const loginController = async =(req,res)=>{
+
+}
+
+
+module.exports ={
+    registerController,
+    loginController
+}
